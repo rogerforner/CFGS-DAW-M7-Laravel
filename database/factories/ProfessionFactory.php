95f70a7b4aba678ee1ee158e795c0975002357c2
Roger Forner Fabre <hello@rogerforner.com>
2018-02-27 12:27:14 +0100

    ProfessionFactory Model Ok

--- database/factories/ProfessionFactory.php
+++ database/factories/ProfessionFactory.php
@@ -2,7 +2,7 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Profession::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence
     ];
