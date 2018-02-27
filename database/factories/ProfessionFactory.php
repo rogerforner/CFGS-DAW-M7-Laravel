93c943c964c3b86a27ea976546ac3979076d0a8d
Roger Forner Fabre <hello@rogerforner.com>
2018-02-27 12:33:41 +0100

    Factory, Model adequat de primeres
    $php artisan make:factory nomFactory --model=ModelAssociat

--- database/factories/ProfessionFactory.php
+++ database/factories/ProfessionFactory.php
@@ -2,6 +2,8 @@
 
 use Faker\Generator as Faker;
 
+// Podríem obtenir el model adequat amb:
+// php artisan make:factory ProfessionFactory --model=Profession
 $factory->define(App\Profession::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(3, false)
