b68f53142c6cb8aed47091e2f0dafe36b508b30e
Roger Forner Fabre <hello@rogerforner.com>
2018-02-27 09:10:49 +0100

    DB, posicionant el nou camp
    Posicionem el camp nou just abans de "password".
    
    $ php artisan migrate:rollback
    $ php artisan migrate

--- database/migrations/2018_02_27_080117_add_profession_to_users.php
+++ database/migrations/2018_02_27_080117_add_profession_to_users.php
@@ -14,7 +14,7 @@ class AddProfessionToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('profession', 50)->nullable();
+            $table->string('profession', 50)->nullable()->after('password');
         });
     }
 
