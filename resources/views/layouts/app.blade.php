a6a8f07b020fd699253dcae427cbf231c0a586b3
Roger Forner Fabre <hello@rogerforner.com>
2018-02-28 09:00:57 +0100

    Usuaris, llistar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,9 +11,70 @@
     <title>{{ config('app.name') }} | @yield('title')</title>
   </head>
   <body>
-    @section('navbar')
-        This is the master sidebar.
-    @show
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="{{ url('/') }}">Botiga</a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <!-- ESQUERRA -->
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Inici <span class="sr-only">(current)</span></a>
+          </li>
+          <!-- Productes -->
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Productes
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              {{-- <a class="dropdown-item" href="{{ url('products') }}">Llistar</a>
+              <a class="dropdown-item" href="{{ url('products/create') }}">Crear</a> --}}
+            </div>
+          </li>
+          <!-- Usuaris -->
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Usuaris
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="{{ url('users') }}">Llistat</a>
+            </div>
+          </li>
+        </ul>
+
+        <!-- DRETA -->
+        {{-- <ul class="navbar-nav flex-row ml-md-auto d-md-flex">
+          <!-- Enllaços autenticació -->
+          @guest
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('login') }}">Accedir</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('register') }}">Registrar-se</a>
+            </li>
+          @else
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                {{ Auth::user()->name }}
+              </a>
+
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                  Tancar sessió
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  {{ csrf_field() }}
+                </form>
+              </div>
+            </li>
+          @endguest --}}
+        </ul>
+      </div>
+    </nav>
 
     <main>
       @yield('content')
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,11 +2,55 @@
 
 @section('title', 'Usuaris')
 
-@section('navbar')
-  @parent
-  <p>This is appended to the master sidebar.</p>
-@endsection
-
 @section('content')
-  <p>This is my body content.</p>
+  <div class="container my-5">
+    <div class="row">
+      <div class="col">
+        <h1 class="display-4">Usuaris</h1>
+      </div><!-- /.row -->
+    </div><!-- /.row -->
+
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <!-- TAULA USUARIS
+            -------------------->
+            <table class="table table-hover">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nom</th>
+                <th scope="col">Correu electrònic</th>
+                <th scope="col">Professió</th>
+                <th scope="col">Accions</th>
+              </tr>
+            </thead>
+            <tbody>
+              @forelse ($users as $user)
+                <tr>
+                  <td class="align-middle">{{ $user->id }}</td>
+                  <td class="align-middle">{{ $user->name }}</td>
+                  <td class="align-middle">{{ $user->email }}</td>
+                  <td class="align-middle">{{ $user->profession }}</td>
+                  <td class="align-middle">
+                    <div class="btn-group" role="group" aria-label="Accions">
+                      <a href="#" class="btn btn-dark btn-sm" role="button" aria-pressed="true">Visualitzar</a>
+                      <a href="#" class="btn btn-warning btn-sm" role="button" aria-pressed="true">Editar</a>
+                      <a href="#" class="btn btn-danger btn-sm" role="button" aria-pressed="true">Eliminar</a>
+                    </div>
+                  </td>
+                </tr>
+              @empty
+                <tr>
+                  <td colspan="5">No hi ha productes a la base de dades.</td>
+                </tr>
+              @endforelse
+            </tbody>
+          </table>
+          </div>
+        </div><!-- /.card -->
+      </div><!-- /.row -->
+    </div><!-- /.row -->
+  </div><!-- /.container -->
 @endsection
